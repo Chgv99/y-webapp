8e55f9b8848509dcb360e76965de26df70a87934
Christian G <christiangv99@gmail.com>
2025-12-26 14:50:19 +0000

    Store token and redirect on successful login

--- src/app/login-form/login-form.ts
+++ src/app/login-form/login-form.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { UserService } from '../core/services/user.service';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-form',
@@ -10,10 +11,14 @@ import { FormsModule } from '@angular/forms';
 })
 export class LoginForm {
   userService = inject(UserService);
+  router = inject(Router);
 
   submitCredentials(username: string, password: string) {
     this.userService.login(username, password).subscribe({
-      next: res => console.log('success', res),
+      next: res => {
+        localStorage.setItem('token', res.token);
+        this.router.navigate(['/home'])
+      },
       error: err => console.error('error', err)
     });
   }
