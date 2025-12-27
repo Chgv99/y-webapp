3af248fbc1851a9f89982ac7ef1b5c12ca284fed
Christian G <christiangv99@gmail.com>
2025-12-27 12:20:45 +0000

    Add feed service

---
+++ src/app/core/services/feed.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@angular/core";
+import { environment } from "../../../environments/environment";
+import { HttpClient } from "@angular/common/http";
+import { ThisReceiver } from "@angular/compiler";
+import { Post } from "../../model/post";
+import { map, Observable } from "rxjs";
+
+@Injectable({ providedIn: 'root' })
+export class FeedService {
+
+    private readonly API = `${environment.apiUrl}`;
+
+    constructor(private http: HttpClient) { }
+
+    getFeed(): Observable<Post[]> {
+        return this.http.get<any[]>(`${this.API}/api/post`).pipe(
+            map(response => response.map(item => new Post(item.message, item.createdAt)))
+        );
+    }
+}
