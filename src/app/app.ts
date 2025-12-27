3a90058bea07db59ab290c6949f964708bf5a130
Christian G <christiangv99@gmail.com>
2025-12-27 12:16:23 +0000

    Clear imports

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,5 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { UserService } from './core/services/user.service';
-import { FormsModule } from '@angular/forms';
-import { LoginForm } from "./login-form/login-form";
 
 @Component({
   selector: 'app-root',
@@ -11,5 +8,5 @@ import { LoginForm } from "./login-form/login-form";
   styleUrl: './app.scss'
 })
 export class App {
-  
+
 }
