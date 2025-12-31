c4f9542c6a577f1dc495e7fbc1888db3f8ef36a4
Christian G <christiangv99@gmail.com>
2025-12-31 00:13:54 +0000

    Home and feed post styling

--- src/app/home/feed/feed-post/feed-post.ts
+++ src/app/home/feed/feed-post/feed-post.ts
@@ -1,9 +1,10 @@
 import { Component, input } from '@angular/core';
 import { Post } from '../../../model/post';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-feed-post',
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './feed-post.html',
   styleUrl: './feed-post.scss',
 })
