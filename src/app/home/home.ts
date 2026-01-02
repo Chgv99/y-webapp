2e0684e01ea7ac4c7e9b3f5d2644ef3df9b9c999
Christian G <christiangv99@gmail.com>
2026-01-02 17:51:07 +0000

    Styling

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -4,10 +4,11 @@ import { PostInput } from "./post-input/post-input";
 import { AuthService } from '../core/services/auth.service';
 import { MatButtonModule } from '@angular/material/button';
 import { Router } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-home',
-  imports: [Feed, PostInput, MatButtonModule],
+  imports: [Feed, PostInput, MatButtonModule, MatToolbarModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
