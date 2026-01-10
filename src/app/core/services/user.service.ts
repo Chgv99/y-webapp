4287adff875fbf8306b2f14ea2783d3e63184324
Christian G <christiangv99@gmail.com>
2026-01-10 11:13:33 +0000

    Get user on demand

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -2,13 +2,18 @@ import { computed, effect, inject, Injectable, PLATFORM_ID } from "@angular/core
 import { User } from "../../model/user";
 import { ApiService } from "./api.service";
 import { AuthService } from "./auth.service";
+import { map, Observable, of } from "rxjs";
+import { Post } from "../../model/post";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({ providedIn: 'root' })
 export class UserService extends ApiService {
     private authService = inject(AuthService);
     private platformId = inject(PLATFORM_ID);
 
-    user = computed(() => {
+    private readonly RESOURCE_URI = '/api/users';
+
+    public currentUser = computed(() => {
         const json = this.authService.parseToken(this.authService.authToken() ?? '')
         if (json) {
             return new User(json.sub, json.username, json.role, new Date(json.createdAt))
@@ -16,7 +21,7 @@ export class UserService extends ApiService {
         return null;
     });
 
-    constructor() {
+    constructor(private http: HttpClient) {
         super();
 
         effect(() => {
@@ -31,4 +36,30 @@ export class UserService extends ApiService {
             }
         })
     }
+
+    getUser(username: String): Observable<User | null> {
+        if (!this.authService.authReady()) {
+            return of(null);
+        }
+
+        return this.http
+            .get<any>(`${this.API_BASE_URL}${this.RESOURCE_URI}?usernames=${username}`)
+            .pipe(
+                map((users: any[]) => users.length > 0 ? new User(users[0].uuid, users[0].username, '', users[0].createdAt) : null)
+            );
+    }
+
+    // PAGINATED
+    // getUser(username: String): Observable<User | null> {
+    //     if (!this.authService.authReady()) {
+    //         return of(null);
+    //     }
+
+    //     return this.http
+    //         .get<any>(`${this.API_BASE_URL}${this.RESOURCE_URI}?usernames=${username}`)
+    //         .pipe(
+    //             map(response => response?.content || []),
+    //             map((users: any[]) => users.length > 0 ? new User(users[0].uuid, users[0].username, '', users[0].createdAt) : null)
+    //         );
+    // }
 }
