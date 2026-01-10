63e625e89a711711fd2134cb3952bdde872f6ba4
Christian G <christiangv99@gmail.com>
2026-01-10 11:18:14 +0000

    Update post api calls

--- src/app/core/services/feed.service.ts
+++ src/app/core/services/feed.service.ts
@@ -1,30 +1,47 @@
 import { HttpClient } from "@angular/common/http";
-import { Injectable, signal } from "@angular/core";
-import { map } from "rxjs";
+import { inject, Inject, Injectable, signal } from "@angular/core";
+import { map, Observable, of } from "rxjs";
 import { Post } from "../../model/post";
 import { ApiService } from "./api.service";
 import { AuthService } from "./auth.service";
+import { UserService } from "./user.service";
 
 @Injectable({ providedIn: 'root' })
 export class FeedService extends ApiService {
-
+    private readonly RESOURCE_URI = '/api/posts';
     feed = signal<Post[]>([]);
 
-    constructor(private http: HttpClient, private authService: AuthService) {
+    constructor(private http: HttpClient, private authService: AuthService, private userService: UserService) {
         super();
     }
 
-    getFeed() {
-        if (!this.authService.authReady()) return;
-        this.http
-            .get<any>(`${this.API}/api/post?page=0&size=10&sort=createdAt,desc`)
+    getFeed(): Observable<Post[]> {
+        if (!this.authService.authReady()) {
+            return of([]);
+        }
+
+        return this.http
+            .get<any>(`${this.API_BASE_URL}${this.RESOURCE_URI}?page=0&size=10&sort=createdAt,desc`)
             .pipe(
                 map(response => response?.content || []),
                 map((posts: Post[]) =>
                     posts.map(post => new Post(post.message, post.author, post.createdAt))
                 )
-            ).subscribe((posts: Post[]) => {
-                this.feed.set(posts);
-            })
+            );
+    }
+
+    getFeedByUser(username: String): Observable<Post[]> {
+        if (!this.authService.authReady()) {
+            return of([]);
+        }
+
+        return this.http
+            .get<any>(`${this.API_BASE_URL}${this.RESOURCE_URI}?authorUsername=${username}&page=0&size=10&sort=createdAt,desc`)
+            .pipe(
+                map(response => response?.content || []),
+                map((posts: Post[]) =>
+                    posts.map(post => new Post(post.message, post.author, post.createdAt))
+                )
+            );
     }
 }
